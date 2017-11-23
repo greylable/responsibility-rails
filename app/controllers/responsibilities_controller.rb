849e181463d3fd21ddbff111772f698b2f61b18d
PingChiang <spclight@gmail.com>
2017-11-23 22:10:12 +0800

    Add Delete function for Responsibility

--- app/controllers/responsibilities_controller.rb
+++ app/controllers/responsibilities_controller.rb
@@ -1,4 +1,5 @@
 class ResponsibilitiesController < ApplicationController
+  before_action :set_responsibility, only: [:destroy]
   def index
     @responsibilities = current_user.responsibilities
   end
@@ -9,19 +10,29 @@ class ResponsibilitiesController < ApplicationController
   end
 
   def create
-    @responsibility = current_user.responsibilities.new(responsibilities_params)
+    @responsibility = current_user.responsibilities.new(responsibility_params)
     if @responsibility.save!
-      redirect_to responsibilities_path, notice: 'Responsibility'
+      redirect_to responsibilities_path, notice: 'Responsibility Created Successfully!'
     else
       render :new
     end
   end
 
+  def destroy
+    @responsibility.destroy
+    redirect_to responsibilities_path, notice: 'Responsibility Deleted Successfully!'
+  end
+
   private
 
-  def responsibilities_params
+  def set_responsibility
+    @responsibility = Responsibility.find(params[:id])
+  end
+
+  def responsibility_params
     params.require(:responsibility).permit(:name, :details, :status, :start_date, :deadline)
   end
 
-end
 
+
+end
